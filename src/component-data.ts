322f6160505be69f0e5ebe2b98c6e31af7f191ed
mai.shu <ansiboy@163.com>
2020-03-04 08:33:43 +0800

    修改 package name

--- src/component-data.ts
+++ src/component-data.ts
@@ -1,6 +1,3 @@
-import { ComponentProps } from "./component-props";
-
-
 export interface ComponentData {
     /** 组件类型名称 */
     type: string;
@@ -8,4 +5,5 @@ export interface ComponentData {
     props?: any;
     /** 子组件 */
     children?: (ComponentData | string)[];
+    id?: string,
 }
--- src/component-props.ts
+++ src/component-props.ts
@@ -1,6 +1,7 @@
 import { ComponentFactory } from "./component-factory";
+import React = require("react");
 
-export interface ComponentProps {
+export type ComponentProps<T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any> = any> = React.ComponentProps<T> & {
     context: any | undefined,
-    factory: ComponentFactory
+    factory: ComponentFactory,
 }
