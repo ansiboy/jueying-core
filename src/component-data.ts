24f1a855c4d97acb83fc65606a55a8dae1153c20
mai.shu <ansiboy@163.com>
2020-03-06 20:01:22 +0800

    update component data

--- src/component-data.ts
+++ src/component-data.ts
@@ -2,8 +2,14 @@ export interface ComponentData {
     /** 组件类型名称 */
     type: string;
     /** 组件属性 */
-    props?: any;
+    props?: {
+        style?: any,
+        ref?: Function
+    };
     /** 子组件 */
     children?: (ComponentData | string)[];
-    id?: string,
+    id?: string;
+    parentId?: string;
+    selected?: boolean;
+    name?:string;
 }
