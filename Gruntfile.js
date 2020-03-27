df3e02e819e7b7adecb717deeaf460a210cdc29e
mai.shu <ansiboy@163.com>
2020-03-27 10:29:12 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 const webpack_src = require('./webpack.config.js');
 
+function modifyVersion() {
+    const package = require("./package.json");
+    const path = require("path");
+
+    let version = package.version || "1.0.0";
+    let arr = version.split(".");
+    arr[arr.length - 1] = (Number.parseInt(arr[arr.length - 1]) + 1).toString();
+    version = arr.join(".");
+    package.version = version;
+
+    const fs = require('fs');
+    let data = JSON.stringify(package, null, 4);
+    fs.writeFileSync("package.json", data, "utf8");
+};
+modifyVersion();
+
 
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
@@ -19,20 +35,3 @@ module.exports = function (grunt) {
     grunt.registerTask('default', ['shell', 'webpack']);
 }
 
-function modifyVersion() {
-    const package = require("./package.json");
-    const path = require("path");
-
-    let version = package.version || "1.0.0";
-    let arr = version.split(".");
-    arr[arr.length - 1] = (Number.parseInt(arr[arr.length - 1]) + 1).toString();
-    version = arr.join(".");
-    package.version = version;
-
-    const fs = require('fs');
-    let data = JSON.stringify(package, null, 4);
-    fs.writeFile("package.json", data, "utf8", (err => {
-        console.log(err)
-    }));
-};
-modifyVersion();
