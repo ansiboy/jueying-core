e09204902b474c7340d5e8b45e3659221fe2ffb0
mai.shu <ansiboy@163.com>
2021-01-12 11:01:00 +0800

    更新 export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export { ComponentData, ComponentProps } from "./component-data";
+export { ComponentData, ComponentProps, PageData } from "./component-data";
 export { ComponentFactory } from "./component-factory";
 export { ComponentInfo } from "./component-info";
 export { component } from "./decorators";
 export { parseComponentData } from "./parse-component-data";
 export { registerComponent, componentTypes } from "./register";
+
+export { Page as Page, PageProps, PageContext } from "./page";
+
+export { PageHeader, PageHeaderProps } from "./page-header";
+export { PageFooter, PageFooterProps } from "./page-footer";
+export { PageBody, PageBodyProps } from "./page-body";
+export { ComponentContainer } from "./component-container";
+import "./style";
