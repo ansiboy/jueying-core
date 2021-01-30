e90173315427c4de62c649012b6b98a319bcea51
mai.shu <ansiboy@163.com>
2021-01-30 09:11:09 +0800

    添加 ComponentContainerProps 导出

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export { parseComponentData } from "./parse-component-data";
 export { registerComponent, componentTypes } from "./register";
 
 export { Page, PageHeader, PageHeaderProps, PageFooter, PageFooterProps, PageBody, PageBodyProps } from "./components/index";
-export { ComponentContainer } from "./component-container";
+export { ComponentContainer, ComponentContainerProps } from "./component-container";
 import "./style";
