451daa85b2d3a3ce86960aa93dc1dfcfa8db474f
mai.shu <ansiboy@163.com>
2021-03-16 15:17:14 +0800

    去掉样式

--- src/components/page-body.tsx
+++ src/components/page-body.tsx
@@ -23,25 +23,23 @@ export class PageBody extends React.Component<PageBodyProps> {
     render() {
         return <PageContext.Consumer>
             {args => {
-                let style: React.CSSProperties = { marginTop: 0 };
-                if (args.pageData != null) {
-                    let header = args.pageData.children.filter(o => o.type == PageHeader.typeName)[0];
-                    let footer = args.pageData.children.filter(o => o.type == PageFooter.typeName)[0];
-                    if (header != null && (header.props as PageHeaderProps).visible) {
-                        let p = header.props as PageHeaderProps;
-                        style.marginTop = p.height;
-                        style.height = `calc(100% - ${p.height}px)`;
-                    }
-                    if (footer != null && (footer.props as PageFooterProps).visible) {
-                        let p = footer.props as PageHeaderProps;
-                        style.marginBottom = p.height;
-                        style.height = `calc(100% - ${(style.marginTop as number) + p.height}px)`;
-                    }
-
-                }
-
-                style.display = this.props.visible ? "" : "none";
-                return <ComponentContainer className={PageBody.className} {...this.props} style={style} />
+                // let style: React.CSSProperties = { marginTop: 0 };
+                // if (args.pageData != null) {
+                //     let header = args.pageData.children.filter(o => o.type == PageHeader.typeName)[0];
+                //     let footer = args.pageData.children.filter(o => o.type == PageFooter.typeName)[0];
+                //     if (header != null && (header.props as PageHeaderProps).visible) {
+                //         let p = header.props as PageHeaderProps;
+                //         style.marginTop = p.height;
+                //         style.height = `calc(100% - ${p.height}px)`;
+                //     }
+                //     if (footer != null && (footer.props as PageFooterProps).visible) {
+                //         let p = footer.props as PageHeaderProps;
+                //         style.marginBottom = p.height;
+                //         style.height = `calc(100% - ${(style.marginTop as number) + p.height}px)`;
+                //     }
+                // }
+                // style.display = this.props.visible ? "" : "none";
+                return <ComponentContainer className={PageBody.className} {...this.props} />
             }}
         </PageContext.Consumer>
     }
