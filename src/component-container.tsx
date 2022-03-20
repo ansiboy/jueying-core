1ba3908b266608dae8b18e409a6af84ea435f8ee
maishu <ansiboy@163.com>
2022-03-20 16:29:23 +0800

    update

--- src/component-container.tsx
+++ src/component-container.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { AppContext } from "./components/context";
 import { parseComponentData } from "./parse-component-data";
-import { CSSProperties } from "react";
 import { ComponentData } from "./component-data";
 import { PageData, registerComponent } from ".";
 
 export interface ComponentContainerProps {
     id: string,
-    className?: string,
-    style?: CSSProperties
+    // className?: string,
+    // style?: CSSProperties,
+    // wrapper?: string,
 }
 
 /** 组件容器，实现组件的渲染 */
@@ -25,9 +25,8 @@ export class ComponentContainer<P extends ComponentContainerProps = ComponentCon
         return <AppContext.Consumer>
             {args => {
                 let children = args.pageData?.children.filter(o => o.parentId == this.props.id) || [];
-                return <div className={this.props.className} style={this.props.style}>
-                    {children.map(o => this.renderChild(o, args.pageData as PageData, args.componentTypes))}
-                </div>
+                return React.createElement(React.Fragment, this.props,
+                    ...children.map(o => this.renderChild(o, args.pageData as PageData, args.componentTypes)))
             }}
         </AppContext.Consumer>
     }
